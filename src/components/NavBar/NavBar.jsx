3cf970110b2461c00ab374fb19a2a8cb9d389d98
Mariano Vargas <marianoluisvargas@gmail.com>
2024-01-29 18:14:38 -0300

    feat(NavBar): added logout button

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,26 @@
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Toolbar, Button } from "@mui/material";
 import styles from "./NavBar.module.css";
+import { useAuthStore } from "../../hooks/useAuthStore";
 
 export const NavBar = () => {
+  const { status, logout } = useAuthStore();
+
   return (
     <>
       <AppBar position='static' className={styles.appBar}>
         <Toolbar>
           <img src='public/firma.svg' alt='logo' className={styles.logo} />
           <div className={styles.spacer} />
+
+          {status === "authenticated" && (
+            <Button variant='outlined' color='inherit' onClick={logout}>
+              Logout
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </>
   );
 };
+
 export default NavBar;
